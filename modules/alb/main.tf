d2c8f23fb627aa58d9a8d722b0b8e262945b5dcb
damilola-ajayi04 <temilolaolusuyi240@gmail.com>
2026-08-01 18:59:42 +0100

    Sprint 5: Healthcheck expanded to Added reusable Application Load Balancer module

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -31,13 +31,14 @@ resource "aws_lb_target_group" "main" {
   target_type = "instance"
 
   health_check {
-
-    path = "/"
-
-    protocol = "HTTP"
-
-    matcher = "200"
-
+    enabled             = true
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
   }
 }
 
